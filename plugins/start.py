1772520d65c4352e22fb8c21bf9e5c42c09d2656
misaghbanimahd <misaghfilm@gmail.com>
2021-05-01 04:51:08 +0430

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "ادامه", callback_data='/help')]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/hel for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
