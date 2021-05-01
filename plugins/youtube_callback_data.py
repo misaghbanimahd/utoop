8b025cff6b97add2b5e591e30f35bab9956dec43
misaghbanimahd <misaghfilm@gmail.com>
2021-05-02 03:34:53 +0430

    Update youtube_callback_data.py

--- plugins/youtube_callback_data.py
+++ plugins/youtube_callback_data.py
@@ -162,7 +162,8 @@ async def send_file(c, q, med, filename):
     finally:
         try:
           thumbnail_url = "https://telegra.ph/file/ce37f8203e1903feed544.png"
-            await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)  
+            await q.edit_message_reply_photo(thumbnail_url, caption=title, reply_markup=buttons)  
+            
             os.remove(filename)
             os.remove(thumb_image_path)
             
