18e27b5418eb3753b2d7203c62bfd90b1e65adb4
misaghbanimahd <misaghfilm@gmail.com>
2021-05-02 04:58:16 +0430

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -46,10 +46,10 @@ async def ytdl(_, message):
         thumb_image_path = f"{output_directory}.jpg"
         im.save(thumb_image_path,"jpeg")
         await message.reply_photo(thumb_image_path, caption=title, reply_markup=buttons)
+        await message.reply_photo("https://telegra.ph/file/ce37f8203e1903feed544.png", caption="hhh", reply_markup=buttons)
         await sentm.delete()
 
-        thumbnail_url = "https://telegra.ph/file/ce37f8203e1903feed544.png"
-            await message.reply_photo(thumbnail_url, caption=title)
+        
     except Exception as e:
         print(e)
         try:
